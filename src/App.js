fb36154295d4cdb125354e601b24cff90536a6b6
aaryan sharma <aaryansharma1976@gmail.com>
2025-02-19 00:56:08 +0530

    Added backend functionality

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import Home from './Pages/Home';
+import Features from './Pages/Features';
+import Blog from './Pages/Blog';
+import About from './Pages/About';
+import FAQ from './Pages/FAQ';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
